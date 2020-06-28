cf811e93bfaa91172108555d2e2d0a6ed76c84d7
dingye1125 <18012327298@163.com>
2020-06-28 11:57:07 +0800

    初始化项目

---
+++ app/src/main/java/dingye38/com/BaseActivity.java
@@ -0,0 +1,11 @@
+package dingye38.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
